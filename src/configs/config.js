f1f52ffce46811b75dc0267f93bcad273779458c
Aaron Chou <33739545+TechDIY@users.noreply.github.com>
2018-06-20 15:45:59 +0800

    Update config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -2,10 +2,10 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkt9fhQz0WFo8iXEDfJGTZcXY3nIgbmDY",
-  authDomain: "login-bad0b",
-  databaseURL: "https://login-bad0b.firebaseio.com",
-  projectId: "project-969730246969",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
 };
 
 export default !firebase.apps.length 
